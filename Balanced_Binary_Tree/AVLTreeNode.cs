6cb47c9b6c5a3af9a276f4e59946736e513c9005
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-27 11:59:08 +0800

    Add project files.

---
+++ Balanced_Binary_Tree/AVLTreeNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Balanced_Binary_Tree
+{
+    class AVLTreeNode<T> : BinaryTreeNode<T>
+        where T : IComparable
+    {
+        public AVLTreeNode(T value)
+            : base(value)
+        {
+        }
+
+        public new AVLTreeNode<T> LeftChild
+        {
+            get
+            {
+                return (AVLTreeNode<T>)base.LeftChild;
+            }
+            set
+            {
+                base.LeftChild = value;
+            }
+        }
+
+        public new AVLTreeNode<T> RightChild
+        {
+            get
+            {
+                return (AVLTreeNode<T>)base.RightChild;
+            }
+            set
+            {
+                base.RightChild = value;
+            }
+        }
+
+        public new AVLTreeNode<T> Parent
+        {
+            get
+            {
+                return (AVLTreeNode<T>)base.Parent;
+            }
+            set
+            {
+                base.Parent = value;
+            }
+        }
+    }
+}
---
+++ Balanced_Binary_Tree/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Balanced_Binary_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new List<int> { 1, 11, 3, 9, 11, 7 };
+            
+            AVLTree<int> abc = new AVLTree<int>();
+
+            foreach (int item in l1)
+            {
+                abc.Add(item);
+
+            }
+
+            var depth = abc.;
+            var tempo = abc.GetInOrderEnumerator();
+
+            Console.WriteLine(tempo.ToString());
+            
+
+        }
+    }
+}
